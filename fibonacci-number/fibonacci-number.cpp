0b46fb9df9fbfe68e82b32496b560dfa9eead455
rakesh9541 <86745045+rakesh9541@users.noreply.github.com>
2023-06-23 09:51:37 +0530

    Time: 16 ms (29.21%) | Memory: 5.9 MB (59.34%) - LeetSync

---
+++ fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int f(int n)
+    {
+        if(n<=1)
+            return n;
+        return f(n-1)+f(n-2);
+    }
+    int fib(int n) {
+        return f(n);
+        
+    }
+};
