09537035e32339611670c6af64a5e9c0b1dac1d7
rakesh9541 <86745045+rakesh9541@users.noreply.github.com>
2023-07-01 14:32:52 +0530

    Time: 142 ms (37.73%) | Memory: 18.5 MB (44.28%) - LeetSync

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -3,29 +3,31 @@ public:
     int coinChange(vector<int>& coins,int amount) 
     {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        vector<int>prev(amount+1,-1);
+        vector<int>curr(amount+1,-1);
         for(int target=0;target<=amount;target++)
         {
            if((target%coins[0])==0)
-            dp[0][target]= target/coins[0];
+            prev[target]= target/coins[0];
            else
-               dp[0][target]=1e8; 
+               prev[target]=1e8; 
         }
 
         for(int index=1;index<n;index++)
         {
             for(int target=0;target<=amount;target++)
             {
-                int notTake=dp[index-1][target];
+                int notTake=prev[target];
                 int take=1e8;
                 if(target>=coins[index])
                 {
-                    take=1+dp[index][target-coins[index]];
+                    take=1+curr[target-coins[index]];
                 }
-                dp[index][target]=min(take,notTake);
+                curr[target]=min(take,notTake);
             }
+            prev=curr;
         }
-        int ans= dp[n-1][amount];
+        int ans= prev[amount];
         if(ans>=1e8) 
             return -1;
         return ans;
