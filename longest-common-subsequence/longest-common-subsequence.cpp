d491601b9a4d903eb129712351d0b3eccb894c48
rakesh9541 <86745045+rakesh9541@users.noreply.github.com>
2023-07-03 11:49:07 +0530

    Time: 54 ms (77.23%) | Memory: 6.5 MB (95.43%) - LeetSync

--- longest-common-subsequence/longest-common-subsequence.cpp
+++ longest-common-subsequence/longest-common-subsequence.cpp
@@ -4,8 +4,6 @@ public:
         int ind1 = text1.size();
         int ind2 = text2.size();
         vector<int> prev(ind2+1, 0), curr(ind2+1, 0);
-        for (int i=0; i<=ind2; i++) prev[i] = 0;
-
         for (int i = 1; i <= ind1; i++) {
             for (int j = 1; j <= ind2; j++) {
                 if (text1[i-1] == text2[j-1]) {
