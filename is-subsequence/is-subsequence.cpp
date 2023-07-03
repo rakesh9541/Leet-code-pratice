ddb66d53d6db2d2ef9130042f6827a61d308acfe
rakesh9541 <86745045+rakesh9541@users.noreply.github.com>
2023-07-04 00:07:34 +0530

    Time: 5 ms (10.77%) | Memory: 6.7 MB (11.09%) - LeetSync

---
+++ is-subsequence/is-subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int ind1 = text1.size();
+        int ind2 = text2.size();
+        vector<int> prev(ind2+1, 0), curr(ind2+1, 0);
+        for (int i = 1; i <= ind1; i++) {
+            for (int j = 1; j <= ind2; j++) {
+                if (text1[i-1] == text2[j-1]) {
+                    curr[j] = 1 + prev[j-1];
+                }
+                else curr[j] = 0 + max(prev[j], curr[j-1]);
+            }
+            prev = curr;
+        }
+        return prev[ind2];
+    }
+    bool isSubsequence(string s, string t) {
+        int a=longestCommonSubsequence(s,t);
+        if(a==s.length())
+            return true;
+        return false;
+        
+    }
+};
